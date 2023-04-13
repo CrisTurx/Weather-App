2bf9a65522210a888c9ba8e36292efcce4b624a1
CrisTurx <cristian.turx@gmail.com>
2023-04-13 20:37:22 +0200

    feat: Weather App

---
+++ WeatherApp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
+import { WeatherService } from './services/weather.service';
+import {FormsModule} from '@angular/forms'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [WeatherService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ WeatherApp/src/app/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+        weatherApiBaseUrl: 'https://yahoo-weather5.p.rapidapi.com/weather',
+        XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+        XRapidAPIHostHeaderValue: 'yahoo-weather5.p.rapidapi.com',
+        XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+        XRapidAPIKeyHeaderValue: '8006f35cddmshbd813c21a43a807p19f4a0jsn2669aa2d8a01',
+
+   };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  
